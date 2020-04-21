e4d9bcfd978dd738b18f766d6a98c26c522c00ba
Kirill Penkin <kirill_486@mail.ru>
2020-04-21 21:53:27 +0400

    library types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,14 +7,18 @@ export enum CsudActions {
   DELETE = 'DELETE',
 }
 
-export interface IEntittyCommon {
+export interface IEntityCommon {
   id: string;
 }
 
-export type IEntity<DomainType> = DomainType & IEntittyCommon;
+export type id = string;
+
+export type IEntity<DomainType> = DomainType & IEntityCommon;
 
 export type IEntityState<Entity extends IEntity<any>> = {[key: string]: Entity };
 
 export interface IAction<Entity extends IEntity<any>> extends Action {
   payload: Entity;
 }
+
+export type EntityFabric<Entity extends IEntity<any>> = () => Entity;
