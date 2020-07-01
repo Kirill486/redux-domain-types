7e134c97d86cb2fe65285d3a9ccc5d0acf9f80ec
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-01 15:12:15 +0400

    simple state controller

--- test/stateController.ts
+++ test/stateController.ts
@@ -1,4 +1,4 @@
-import { command, ClientSelector } from "../src/definitions";
+import { command, ClientSelector, id, HashIndex } from "../src/definitions";
 
 export interface IStateInstanceController<State> {
     initial: ClientSelector<State>;
@@ -11,9 +11,16 @@ export interface IStateCommandController<State> {
 
 export interface IStateQueryController<State> {
     select: ClientSelector<Partial<State>>;
+    queryKeys: ClientSelector<id[]>;
 }
 
 export interface IStateController<State> extends
 IStateInstanceController<State>,
 IStateCommandController<State>,
 IStateQueryController<State> {}
+
+export type TStateControllerProvider<State> =
+new (
+    propertyTitle: string,
+    initial: State,
+) => IStateController<State>;
