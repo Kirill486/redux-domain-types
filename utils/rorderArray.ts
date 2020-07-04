0819c6ed1b317be2e5e29be889370a8b9ab49f20
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-04 13:18:55 +0400

    better to understand does not mutate returns a new instance

--- utils/rorderArray.ts
+++ utils/rorderArray.ts
@@ -3,5 +3,16 @@ const moveItem = (
     from: number,
     to: number,
 ) => {
-    return array.splice(to, 0, array.splice(from, 1)[0]);
-}
+    return array.map((item, index, initial) => {
+        const isFromIndex = index === from;
+        const isToIndex = index === to;
+        
+        if (isFromIndex) {
+            return initial[to];
+        } else if (isToIndex) {
+            return initial[from];
+        } else {
+            return item;
+        }
+    });
+};
