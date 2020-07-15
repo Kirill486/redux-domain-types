a68297a63ea0dd08da6220549b38a1f038914ac2
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-15 14:11:32 +0400

    some green tests

--- src/RecordStateController/RecordStateController.ts
+++ src/RecordStateController/RecordStateController.ts
@@ -1,28 +1,63 @@
 import {IRecordStateController} from '../../api_describtion/recordStateController';
-import { command, ClientSelector, id, Factory, IRecordState } from '../../utils/definitions';
+import { ClientSelector, id, IRecordState } from '../../utils/definitions';
 import { StateControllerBlueprint } from '../IExtendReduxApi/StateControllerBlueprint';
 import { Store } from 'redux';
+import { ReduxStateController } from '../StateController/StateController';
+import { KeyDoesNotExistToDelete, RecordValueCannotBeUndefined } from '../exceptions';
 
 export class ReduxRecordStateController<Record>
 extends StateControllerBlueprint<IRecordState<Record>>
 implements IRecordStateController<Record> {
-    
+
+    controller: ReduxStateController<IRecordState<Record>>;
+
     constructor(
         propertyTitle: string,
     ) {
         super(propertyTitle);
 
-        // add checks that factory does not return undefined
-        // use null instead
+        const initialState: IRecordState<Record> = {};
+        this.controller = new ReduxStateController(propertyTitle, initialState);
     }
     
-    makeReducer = () => null;
-    plugIn = ({dispatch, getState}: Store<any>) => {
-        this.basePlugIn(dispatch, getState);
+    makeReducer = () => {
+        return this.controller.makeReducer();
     };
 
-    set: command;
-    delete: command;
+    plugIn = (store: Store<any>) => {
+        this.controller.plugIn(store);
+        this.basePlugIn(store.dispatch, store.getState);
+    };
+
+    set = (id: id, data: Record) => {
+
+        const recordPresent = ((typeof data) !== "undefined");
+
+        if (recordPresent) {
+            const state = this.controller.select();
+            state[id] = data;
+            const newState = {...state};
+            this.controller.set(newState);
+        } else {
+            throw RecordValueCannotBeUndefined(id);
+        }
+
+        
+    };
+
+    delete = (id: id) => {
+        const state = this.controller.select();
+
+        const recordPresent = ((typeof state[id]) !== "undefined");
+
+        if (recordPresent) {
+            delete state[id];
+            const newState = {...state};
+            this.controller.set(newState);
+        } else {
+            throw KeyDoesNotExistToDelete(id);
+        }        
+    };
 
     select: ClientSelector<Record>;
 }
--- src/exceptions.ts
+++ src/exceptions.ts
@@ -1,3 +1,5 @@
+// State Controller
+
 export const StateControllerUnknownPropertyName = (propertyKey: string) => new Error(`Can not select property ${propertyKey} It does not exist`);
 export const StateControllerUnknownPropertyNames = (propertyKeys: string[]) =>
 new Error(
@@ -9,3 +11,10 @@ export const StateControllerUnknownRootPropertyName = (propertyName: string) => 
 export const NoRootSelectorProvided = () => new Error('NoRootSelectorProvided');
 
 export const UnpluggedControllerOperation = (propertyName: string) => Error(`Controller you're using for ${propertyName} is unplugged`);
+
+// Record State controller
+
+export const RecordValueCannotBeUndefined = (recordKey: string) => new Error(`Attempt to set ${recordKey} to undefined. Use delete instead`);
+export const KeyDoesNotExistToDelete = (recordKey: string) => new Error(`Record with key ${recordKey} does not exist. Cannot select`);
+
+export const KeyYouReTryingToReachDoesNotExist = (recordKey: string) => new Error(`Record with key ${recordKey} does not exist. Cannot select`);
