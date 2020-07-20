30444bb5c52f0f16a3cd372c72d8043106a4060e
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-20 12:48:17 +0400

    tree tests

--- tests/EntityStateController/tree.ts
+++ tests/EntityStateController/tree.ts
@@ -1,8 +1,12 @@
 import * as assert from 'assert';
-import {createRBTree, RedBlackTree} from '../../node_modules/search-tree/rbtreeTree';
+import * as SearchTree from '../../node_modules/search-tree/dist/rbtree';
 import { IProduct } from '../../domain_types/domainTypes';
 import { IIndex } from '../../utils/definitions';
 import { randomInt } from '../../utils/randomInt';
+import { ITree } from 'search-tree/libraryDefinitions';
+import { Timer } from '../../utils/timer';
+import { time } from 'console';
+import { roughlyEqual } from '../../utils/roughlyEqual';
 // Here we have our tree tested.
 // It should return pointer to the first element that satisfy query fast
 // And then return elements even faster
@@ -25,9 +29,11 @@ import { randomInt } from '../../utils/randomInt';
 // * search for a single element by query
 // * search for a query
 
-const initializeDataset = () => {
-    const complexityMultiplier = 1000000;
-    const prods = [];
+const timer = new Timer();
+const commonComplexityMultiplier = 1000000;
+
+const initializeDataset = (complexityMultiplier) => {    
+    const prods: IIndex<IProduct>[] = [];
     for (let t = 0; t < complexityMultiplier; t++) {
         const id = randomInt(0, complexityMultiplier);
         const makeUpProd: IIndex<IProduct> = {
@@ -42,22 +48,121 @@ const initializeDataset = () => {
     return prods;
 }
 
-const initializeTree = () => {
-    const dataSet = initializeDataset();
-    let tree = createRBTree<IIndex<IProduct>>();
+const initializeTree = (commonComplexityMultiplier = 1000000) => {
+    const dataSet = initializeDataset(commonComplexityMultiplier);
+    let tree: ITree<IIndex<IProduct>> = SearchTree.createRBTree<IIndex<IProduct>>();
     dataSet.forEach((prod:IIndex<IProduct>) => {
-        tree = tree.insert(prod.id, prod) as RedBlackTree<IProduct>;
+        tree = tree.insert(prod.id, prod) as ITree<IIndex<IProduct>>;
     });
+    return tree;
 }
 
+timer.start();
+
+const tree1 = initializeTree();
+// const tree2 = initializeTree(commonComplexityMultiplier * 100);
+
+const initTime = timer.stop();
+console.log(`init tree time ${initTime}`);
+
 describe('Tree is fast', () => {
-    it('tree is fast to insert', () => {
-        const objectToCompare = {};
+    it('tree is fast to insert but worse then object', (done) => {
+        timer.start();
+        const tree = initializeTree();
+        const treeInsert = timer.stop();
         
+        timer.start();
+        const objectToCompare = {};
+        const dataSet = initializeDataset(commonComplexityMultiplier);
+        dataSet.forEach((item) => {
+            objectToCompare[item.id] = item;
+        });
+        const objectInsert = timer.stop();
 
+        timer.start();
+        const arrayToCompare = [];
+        dataSet.forEach((item) => {
+            arrayToCompare.push(item);
+        });
+        const arrayInsert = timer.stop();
 
+        console.log(`Insert benchmark tree: ${treeInsert} object: ${objectInsert} array: ${arrayInsert}`);
+        assert.ok(treeInsert  > objectInsert);
+        done();
     });
-    it('tree is fast to search', () => {});
-    it('tree is fast to return sequence', () => {});
 
+    it('tree is fast to search', () => {
+        timer.start();
+        for (let t = 0; t< commonComplexityMultiplier; t++) {
+            const val = tree1.find(randomInt(0, commonComplexityMultiplier));
+        }
+        const treeSearch = timer.stop();
+        console.log(`Tree search: ${treeSearch}`);
+    });
+
+    it('tree is fast to search key like', () => {
+        timer.start();
+        const timeDelation = 10;
+        const lightComplexityMultiplier = commonComplexityMultiplier / timeDelation;
+
+        for (let t = 0; t< lightComplexityMultiplier; t++) {
+            tree1.lt(randomInt(0, commonComplexityMultiplier));
+        }
+
+        const findKeyLess = timer.stop();
+        console.log(`Tree conditional search: ${findKeyLess * timeDelation}`);
+    });
+
+    it('tree is fast to return sequence', (done: any) => {
+        // https://en.wikipedia.org/wiki/AVL_tree#Traversal
+        // Our tree returns next and previos element for a constant time.
+
+        const idToStart = randomInt(0, commonComplexityMultiplier / 2);
+        const timeDelation = 1000;
+        const lightComplexityMultiplier = commonComplexityMultiplier / timeDelation;
+
+        timer.start();
+
+
+        const iter1 = tree1.lt(idToStart);
+        // const iter2 = tree2.lt(idToStart);
+
+        const searchTime = timer.stop();
+
+        console.log(`searchTime: ${searchTime}`);
+
+        assert.ok(iter1);
+        // assert.ok(iter2);
+
+        timer.start();
+
+        // Case1
+
+        for (let t = 0; t < lightComplexityMultiplier / 2; t++) {
+            iter1.next();
+            const val = iter1.value;
+            assert.ok(val);
+        }
+
+        const tree1Time = timer.stop();
+
+
+        // Case2
+
+        // timer.start();
+
+        // for (let t = 0; t < lightComplexityMultiplier / 2; t++) {
+        //     iter2.next();
+        //     const val = iter1.value;
+        //     assert.ok(val);
+        // }
+
+        // const tree2Time = timer.stop();
+        console.log(`Sequence1: ${tree1Time} Sequence2: ${1}`);
+
+        assert.ok(roughlyEqual(tree1Time, 1));
+        done();
+    });
+
+    // it('tree ')
 });
