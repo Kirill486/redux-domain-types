d655e1a620bd30c3d7563e291b16f4ba20977c00
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-22 18:33:54 +0400

    one more init

--- tests/api/constants.ts
+++ tests/api/constants.ts
@@ -126,6 +126,20 @@ export const initializeStoreWithProductStateController = () => {
     }
 }
 
+export const initializeStoreWithPositionStateController = () => {
+    const PositionEntityStateController = initializePositionEntityStateController();
+    const reducerMap: ReducerMappedToProperty<any> = PositionEntityStateController.makeReducer();
+    const combinedReducer = combineReducers({...reducerMap});
+    const store = createStore(combinedReducer);
+
+    PositionEntityStateController.plugIn(store);
+
+    return {
+        store,
+        controller: PositionEntityStateController,
+    }
+}
+
 export const initializeStoreWithProductStateControllerAndData = () => {
     const {store, controller} = initializeStoreWithProductStateController();
         const newProd1: IEntity<IProduct> = {
@@ -144,3 +158,30 @@ export const initializeStoreWithProductStateControllerAndData = () => {
     controller.add(prods);
     return {store, controller, prods};
 }
+
+// Looks like a Use case for Pool here:
+// -- combine properiest reducers
+// -- manage pointer safety
+// -- manage index pointer sefety
+
+// export const initializeStoreWithPositionStateControllerAndData = () => {
+//     const {store: prosuctStore, prods, controller: prodsController} = initializeStoreWithProductStateControllerAndData();
+//     const {store: positionStore, controller: positionController} = initializeStoreWithPositionStateController();
+    
+//     const position1: IEntity<IPosition> = {
+//         id: '12345',
+//         amount: 55,
+//         product: prods[0].id,
+//     }
+
+//     const position2: IEntity<IPosition> = {
+//         id: '123452354',
+//         amount: 552,
+//         product: prods[1].id,
+//     }
+
+//     const positions = [position1, position2];
+//     positionController.add(positions);
+
+//     return {positionStore, positionController, positions, prosuctStore, prods, prodsController};
+// }
