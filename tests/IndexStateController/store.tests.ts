34a4feee782480a449cd1556c5635a8232b4febd
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-25 14:31:19 +0400

    tests with no implementation yet

---
+++ tests/IndexStateController/store.tests.ts
@@ -0,0 +1,27 @@
+describe('IndexStateController can store index data', () => {
+
+    it('can add single id to index', () => {
+
+    });
+
+    it('can add multiple id to the same index', () => {
+        
+    });
+
+    it('can add multiple ids to different indexes', () => {
+        
+    });
+
+    it('can delete id from index', () => {
+        
+    });
+
+    it('can delete multiple ids from index', () => {
+        
+    });
+
+    it('will throw on deleting by hash that not exist', () => {
+        
+    });
+
+});
