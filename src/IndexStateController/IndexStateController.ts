4137900c3e87dea407f2698ee82fe5996d93a4cd
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-26 15:45:02 +0400

    index state controller implementation

--- src/IndexStateController/IndexStateController.ts
+++ src/IndexStateController/IndexStateController.ts
@@ -1,20 +1,82 @@
 import { StateControllerBlueprint } from "../IExtendReduxApi/StateControllerBlueprint";
-import {IIndexStateController, hash} from '../../api_describtion/indexStateController';
+import {IIndexStateController, hash, entities} from '../../api_describtion/indexStateController';
 import { id } from "../../utils/definitions";
+import { RedBlackTree, defaultCompare } from 'search-tree';
+import { ReduxStateController } from "..";
+import { deepClone } from "../../utils/deepClone";
+import { INode } from "search-tree/dist/libraryDefinitions";
+
+export type IndexInnerTreeType = INode<entities> | null;
 
 export class IndexStateController
-extends StateControllerBlueprint<any>
+extends StateControllerBlueprint<IndexInnerTreeType>
 implements IIndexStateController {
+
+    controller: ReduxStateController<IndexInnerTreeType>;
 
     constructor(propertyTitle) {
         super(propertyTitle);
+
+        this.controller = new ReduxStateController<IndexInnerTreeType>(this.propertyTitle, null);
     }
 
-    makeReducerInner = () => null;
+    get InnerTreeCopy(): IndexInnerTreeType {
+        return deepClone(this.controller.select() as INode<entities>);
+    }
 
-    add = (hash: hash, ids: id[]) => undefined;
-    remove = (hash: hash, ids: id[]) => undefined;
+    get InnerTreePointer(): IndexInnerTreeType {
+        return this.controller.select() as INode<entities>;
+    }
 
-    select = (hashFrom: hash, hashTo: hash) => ["42"];
-    includes = () => false;
+    makeReducerInner = () => {
+        return this.controller.makeReducerInner();
+    };
+
+    add = (hash: hash, ids: id[]) => {
+        let tree = new RedBlackTree<entities>(defaultCompare, this.InnerTreeCopy);
+        const iterator = tree.find(hash);
+        if (iterator) {
+            const {value} = iterator;
+            tree = iterator.update([...value, ...ids]);
+        }
+        const nextInnerTree = tree.root;
+        this.controller.set(nextInnerTree);        
+    };
+    remove = (hash: hash, ids: id[]) => {
+        let tree = new RedBlackTree<entities>(defaultCompare, this.InnerTreeCopy);
+        const iterator = tree.find(hash);
+
+        if (iterator) {
+            const { value } = iterator;
+            const nextValue = value.reduce((accumulator, item)  => {
+                if (!ids.includes(item)) {
+                    accumulator.push(item)
+                }
+                return accumulator;
+            }, []);
+            tree = iterator.update(nextValue);
+            const nextInnerTree = tree.root;
+            this.controller.set(nextInnerTree);
+        }
+    };
+
+    select = (hashFrom?: hash, hashTo?: hash) => {
+        const tree = new RedBlackTree<entities>(defaultCompare, this.InnerTreePointer);
+        const iterator = tree.begin;
+        const result: entities = [];
+        while (iterator.hasNext) {
+            iterator.next();
+            const entities = iterator.value;
+
+            entities.forEach((id: id) => result.push(id));
+        }
+
+        return result;
+    };
+
+    includes = (hash: hash) => {
+        const tree = new RedBlackTree<entities>(defaultCompare, this.InnerTreePointer);
+        const iterator = tree.find(hash);
+        return iterator.valid;
+    };
 }
