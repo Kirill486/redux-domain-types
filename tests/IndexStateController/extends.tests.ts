e135f82e1a42d310f5a84ff78d7195a437855356
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-26 14:46:29 +0400

    extends

--- tests/IndexStateController/extends.tests.ts
+++ tests/IndexStateController/extends.tests.ts
@@ -1,12 +1,18 @@
 import * as assert from 'assert';
+import { initializeIndexStateController } from '../api/constants.index';
 
 describe('IndexStateController extends redux API', () => {
 
     it('has makeReducer method that returns an object with key and reducer props', () => {
+        const indexStateController = initializeIndexStateController();
+        const reducerProperty = indexStateController.makeReducer();
 
+        const reducerKey = Object.keys(reducerProperty);
+        assert.equal(reducerKey.length, 1);
+
+        const reducer = reducerProperty[reducerKey[0]];
+        assert.equal(typeof reducer, "function");
     });
 
-    it('reducer can be executed with state and it will return state', () => {
-        
-    });
+    // As soon as we get some understanding of the tree we're gonna store
 });
