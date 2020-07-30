98d18bd05b40a9a2bee3687fd058c23495547b23
Kirill Penkin <lambda.function.486@gmail.com>
2020-07-30 12:12:14 +0400

    lint

--- api_describtion/libraryApi.ts
+++ api_describtion/libraryApi.ts
@@ -8,14 +8,14 @@ export type AnyStateController = IEntityStateController<any> | IStateController<
 // * complex indexes (it provides a selector by id to inner controllers)
 // * pointer safety (deletes links that point to undefined)
 
-export interface StateControllerPool extends IExtendReduxAPI<any> {    
+export interface StateControllerPool extends IExtendReduxAPI<any> {
     getControllerFor: (property: string) => AnyStateController;
 }
 
-export interface IExtendReduxAPI<State> {    
+export interface IExtendReduxAPI<State> {
     propertyTitle: string;
     getControllerProperty: () => State;
-    
+
     makeReducer: Factory<ReducerMappedToProperty<State>>;
     plugIn: (commandEntryPoint: command, rootGetStateSelector: ClientSelector<State>) => void;
     isPlugged: () => boolean;
