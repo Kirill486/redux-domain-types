e19afcb2cb5a3189c6e2fb428624cb4e5e7c64a3
Kirill Penkin <lambda.function.486@gmail.com>
2020-08-07 12:30:49 +0400

    dummies to add more tests

---
+++ tests/StateControllerPool/index.complex.modify.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+describe('ControllerPool modify complex indexes indexes', () => {
+    it('when you add a position, dependent index is add', () => {
+        
+    });
+});
