e285a71c7e63775e36195bc6e9de53e2a32c8764
Kirill Penkin <lambda.function.486@gmail.com>
2020-08-08 12:56:31 +0400

    first lines of pool code

--- src/StateControllerPool/StateControllerPool.ts
+++ src/StateControllerPool/StateControllerPool.ts
@@ -1,32 +1,69 @@
-import { AnyStateController } from "../../api_describtion/libraryApi";
+import { AnyStateController, ReducerMappedToProperty } from "../../api_describtion/libraryApi";
 import { StateControllerBlueprint } from "../IExtendReduxApi/StateControllerBlueprint";
 import { StateControllerPool } from "../../api_describtion/StateControllerPool";
+import { DuplicateProperyTitle, CannotGetControllerForKey } from "./exceptions";
+import { ReduxEntityStateController } from "..";
+import { ReduxStateController } from "../StateController/StateController";
+import { combineReducers } from "redux";
 
 export class ReduxStateControllerPool extends StateControllerBlueprint<any> implements StateControllerPool {
+    stateControllers: { [priopertyTitle: string]: AnyStateController }
+
     constructor(
         properyTitle: string,
         ...stateControllers: AnyStateController[]
     ) {
         super(properyTitle);
-        // nothingYet
+
+        stateControllers.forEach((controller) => {
+            const stateControllersKeys = Object.keys(this.stateControllers);
+
+            if (stateControllersKeys.includes(controller.propertyTitle)) {
+                throw DuplicateProperyTitle(controller.propertyTitle)
+            }
+
+            this.stateControllers[controller.propertyTitle] = controller;
+        });
     }
+
     entityChanged: (propertyTitle: string, id: any) => void;
     stateChanged: (propertyTitle: string) => void;
 
     get controllersKeys() {
-        return [] as string[];
+        return Object.keys(this.stateControllers);
     };
 
     get entityControllersKeys() {
-        return [] as string[];
+        const controllerKeys = this.controllersKeys;
+        const entityControllerKeys = controllerKeys.filter((key) => this.stateControllers[key] instanceof ReduxEntityStateController)
+        return entityControllerKeys;
     }
 
     get stateControllersKeys() {
-        return [] as string[];
+        const controllerKeys = this.controllersKeys;
+        const entityControllerKeys = controllerKeys.filter((key) => this.stateControllers[key] instanceof ReduxStateController)
+        return entityControllerKeys;
     }
 
-    makeReducerInner: () => null;
+    makeReducerInner = () => {
+        let result: ReducerMappedToProperty<any> = {};
+        this.controllersKeys.forEach((key) => {
+            result = {
+                ...result,
+                ...this.stateControllers[key].makeReducer()
+            };
+        });
+        return combineReducers(result);
+    };
 
-    getControllerFor: (property: string) => null;
+    getControllerFor = (property: string) => {
+        const validKey = this.controllersKeys.includes(property);
+        if (validKey) {
+            const correspondingController = this.stateControllers[property];
+            return correspondingController;
+        } else {
+            throw CannotGetControllerForKey(property);
+        }
+    };
 
 }
